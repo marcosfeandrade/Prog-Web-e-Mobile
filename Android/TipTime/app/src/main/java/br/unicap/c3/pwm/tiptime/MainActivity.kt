70d1ce568534c1a083ef4b768a2b99959d08a352
marcosfandrade <marcos.fellipe2211@gmail.com>
2022-04-28 22:38:42 -0300

    Exercicio 1 Android

---
+++ Android/TipTime/app/src/main/java/br/unicap/c3/pwm/tiptime/MainActivity.kt
@@ -0,0 +1,37 @@
+package br.unicap.c3.pwm.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.unicap.c3.pwm.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+    }
+
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
+    }
+}
